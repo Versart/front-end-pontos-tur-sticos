3936a85f599bd59aee5ade9a0ad135a5a26e3c09
Versart <wesleyversart@gmail.com>
2024-08-04 20:52:02 -0300

    subindo o projeto

--- src/app/ponto-turistico-detalhe/ponto-turistico-detalhe.component.ts
+++ src/app/ponto-turistico-detalhe/ponto-turistico-detalhe.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import {GoogleMap} from '@angular/google-maps';
+import { environment } from '../../environments/environment.development';
+
+
+
 
 interface PontoTuristico {
   id: string;
@@ -14,16 +19,22 @@ interface PontoTuristico {
 @Component({
   selector: 'app-ponto-turistico-detalhe',
   standalone:true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, GoogleMap],
   templateUrl: './ponto-turistico-detalhe.component.html',
   styleUrls: ['./ponto-turistico-detalhe.component.css']
 })
 export class PontoTuristicoDetalheComponent implements OnInit {
   pontoTuristico: PontoTuristico | undefined;
 
+  center: google.maps.LatLngLiteral = {lat: 21, lng: 12};
+  zoom = 4;
+  display?: google.maps.LatLngLiteral;
+  
+
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
+    this.center = {lat: Number(this.pontoTuristico?.latitude), lng: Number(this.pontoTuristico?.longitude)}
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.http.get<PontoTuristico>(`https://pontos-turisticos-backend.onrender.com/pontosturisticos/${id}`)
@@ -31,5 +42,12 @@ export class PontoTuristicoDetalheComponent implements OnInit {
           this.pontoTuristico = data;
         });
     }
+    
   }
+
+ 
+
+  
+  
+  
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    API_KEY_GOOGLE: "AIzaSyCUmK6Tw4Ab03nOMOWbci58BP3i7QfiDQk"
+};
